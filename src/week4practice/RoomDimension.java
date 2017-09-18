e0b89b4f4bf0688928e062128355c1454f1e126b
mjmersenski <mjmersenski@10.204.56.240>
2017-09-18 09:10:49 -0500

    initial git commit

---
+++ src/week4practice/RoomDimension.java
@@ -0,0 +1,5 @@
+package week4practice;
+
+public class RoomDimension {
+    
+}
---
+++ src/week4practice/Week4Practice.java
@@ -0,0 +1,9 @@
+package week4practice;
+
+public class Week4Practice {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
